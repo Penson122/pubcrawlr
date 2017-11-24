04725286f9d5dc18a7f6c2b5ccf37b3de2aafd1b
SamphireReeve <samphire.reeve4@gmail.com>
2017-11-24 14:31:07 +0000

    Trello 4: Added planner component to plan route.

--- routes/Plan.js
+++ routes/Plan.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import Planner from '../components/Planner';
 
 class Plan extends React.Component {
-  static navigationOptions = {
-    title: 'Plan your route',
-  };
   render(){
-    const { navigate } = this.props.navigation;
     return (
-      <Text>Plan!</Text>
+      <Planner />
     )
   }
 };
