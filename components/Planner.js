1b247938b679a26a1b7087b87bb06361f73e1a4c
SamphireReeve <samphire.reeve4@gmail.com>
2017-11-24 14:27:55 +0000

    Trello 4: Added basic component look for New crawl survey view.

---
+++ components/Planner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Card, Button } from 'react-native-elements';
+import { Slider } from 'react-native-elements';
+
+class Planner extends React.Component {
+  render() {
+    function someFunction(){
+
+    }
+    return (
+      <Card>
+          <Text h3>How many pubs? </Text>
+          <FormInput onChangeText={someFunction}/>
+          <Text>{"\n"}</Text>
+
+          <Text h3>How many people? </Text>
+          <FormInput onChangeText={someFunction}/>
+          <Text>{"\n"}</Text>
+
+          <Text h3>Start Time: </Text>
+          <FormInput onChangeText={someFunction}/>
+          <Text>{"\n"}</Text>
+
+          <Text h3>Starting destination: </Text>
+          <FormInput onChangeText={someFunction}/>
+          <Text>{"\n"}</Text>
+
+          <Text h3>Final destination: </Text>
+          <FormInput onChangeText={someFunction}/>
+          <Text>{"\n"}</Text>
+
+          <Button raised icon={{name: 'check'}} title='Find my route!' />
+      </Card>
+    );
+  }
+}
+
+export default Planner;
