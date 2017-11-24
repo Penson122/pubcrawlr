92d46e829aece793fe82da65cbb625500c36942e
jpenson <jpenson@uea.ac.uk>
2017-11-24 15:49:41 +0000

    routing with polylines implemented

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
+
+import ActionButton from './ActionButton'
 
 import { MapView } from 'expo';
 
@@ -16,22 +18,62 @@ class Map extends React.Component {
   }
   render(){
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: this.state.initialRegion.latitude,
-          longitude: this.state.initialRegion.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: this.state.initialRegion.latitude,
+            longitude: this.state.initialRegion.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+        {this.props.markers.map((marker, i) => {
+          return (
+          <MapView.Marker key={i}
+            coordinate={marker.location}
+          >
+            <Tooltip name={marker.name} description={marker.description} onPress={() => this.props.onMarkerPress(marker.location)}/>
+          </MapView.Marker>);
+        })}
+          <MapView.Polyline coordinates={this.props.polylines}/>
+        </MapView>
+        <View style={styles.button}>
+        <Button title='Done' onPress={this.props.onDone}/>
+        </View>
+      </View>
     )
   }
 }
 
+const Tooltip = ({name, description, onPress}) => {
+  return (
+    <MapView.Callout>
+    <View>
+      <View>
+        <Text>{name}</Text>
+      </View>
+      <Text>{description}</Text>
+      <View>
+        <ActionButton size={15} color='green' onPress={onPress} />
+      </View>
+    </View>
+    </MapView.Callout>
+  )
+}
+
+
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  button: {
+    marginBottom: '10%'
   }
 });
 
